e095fb5bd7b0a3d500a6391e2a562c4c3515d704
MM871 <100428910+MM871@users.noreply.github.com>
2024-01-04 19:49:36 -0500

    first work
    pretty cool

---
+++ script.js
@@ -0,0 +1,25 @@
+document.getElementById("about").addEventListener("click",about);
+document.getElementById("faq").addEventListener("click",faq);
+document.getElementById("whoweare").addEventListener("click",whoweare);
+document.getElementById("examples").addEventListener("click",examples);
+document.getElementById("lessons").addEventListener("click",lessons);
+
+function about(){
+    //scroll down to about on main page
+}
+
+function faq(){
+    //scroll down to FAQ on main page
+}
+
+function whoweare(){
+    //scroll down to who we are section on main poge
+}
+
+function examples(){
+    //uuuuuhhhhh
+}
+
+function lessons(){
+    location.assign("lessons.html");
+}
