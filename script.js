4fa1634c24bd443f1b779e38f273e8f7003f9918
JasonEl1 <87441196+JasonEl1@users.noreply.github.com>
2024-01-15 22:07:19 -0500

    scroll and hover animation on main page

--- script.js
+++ script.js
@@ -1,25 +1,28 @@
-document.getElementById("about").addEventListener("click",about);
-document.getElementById("faq").addEventListener("click",faq);
-document.getElementById("whoweare").addEventListener("click",whoweare);
-document.getElementById("examples").addEventListener("click",examples);
-document.getElementById("lessons").addEventListener("click",lessons);
+document.getElementById("about").addEventListener("click", about);
+document.getElementById("faq").addEventListener("click", faq);
+document.getElementById("whoweare").addEventListener("click", whoweare);
+document.getElementById("examples").addEventListener("click", examples);
+document.getElementById("lessons").addEventListener("click", lessons);
 
-function about(){
-    //scroll down to about on main page
+function about() {
+  //scroll down to about on main page
+  document.getElementById("aboutdiv").scrollIntoView({ behavior: "smooth" });
 }
 
-function faq(){
-    //scroll down to FAQ on main page
+function faq() {
+  //scroll down to FAQ on main page
+  document.getElementById("faqdiv").scrollIntoView({ behavior: "smooth" });
 }
 
-function whoweare(){
-    //scroll down to who we are section on main poge
+function whoweare() {
+  //scroll down to who we are section on main poge
+  document.getElementById("whowearediv").scrollIntoView({ behavior: "smooth" });
 }
 
-function examples(){
-    //uuuuuhhhhh
+function examples() {
+  //uuuuuhhhhh
 }
 
-function lessons(){
-    location.assign("lessons.html");
+function lessons() {
+  location.assign("lessons.html");
 }
